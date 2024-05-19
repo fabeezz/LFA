fac21164f26a3df78646a4d31162b3d67dc6af0b
Fabrizzio Jiglau <fabrizziojiglau2004@gmail.com>
2024-05-19 23:29:32 +0300

    /regular_grammar

---
+++ Tema2/gramatici_regulate.py
@@ -0,0 +1,33 @@
+
+def cuvinte(n):
+    word_list = []
+    for start in dict['S']:
+        word_list.append(start)
+
+    while n>1:
+        word_list_new = []
+        length = len(word_list)
+        for word in word_list:
+            for index in range(len(word)):
+                if word[index] in dict.keys():
+                    for prod in dict[word[index]]:
+                        word_list_new.append(f'{word[:index]}{prod}')
+        word_list = word_list_new
+        n -= 1
+
+    with open("outputfile.txt", "w") as g:
+        for word in word_list:
+            if word.islower():
+                g.write(f'{word} ')
+
+with open("inputfile.txt") as f:
+    dict = {}
+    for line in f:
+        temp = line.strip().split()
+        dict[temp[0]] = []
+        for elem in temp[1:]:
+            if elem != "->" and elem != "|":
+                dict[temp[0]].append(elem)
+
+n = int(input("Lungimea cuvintelor: "))
+cuvinte(n)
